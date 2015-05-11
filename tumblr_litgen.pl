558dd1bb65c4fc1a29574ba9c18d2e2c4722db70
William B. Vanti <william.vanti@gmail.com>
2015-05-11 16:16:32 -0400

    arrays for bib elements

--- tumblr_litgen.pl
+++ tumblr_litgen.pl
@@ -6,38 +6,64 @@ use BibTeX::Parser;
 
 open (my $IF, "<", "bibtex_test_utf8.txt") or die "Could not open input $!\n";
 open (my $OF, ">", "tumblr_out2.txt") or die "Could not open output $!\n";
+open (my $TF, ">", "testfile.txt") or die "Nope $!";
 
 my $parser = BibTeX::Parser->new($IF);
 
 createBeginning();	# starts the file, stops just before needing authors
 
+my $counter = 0;
+my @title;
+my @journal;
+my @doi;
+my @volume;
+my @number;
+my @authorList;
+
 while (my $entry = $parser->next) {
 	if ($entry->parse_ok) {
 		my $type    = $entry->type;
-		my $title   = $entry->field("title");
-		my $journal = $entry->field("journal");
-		my $doi     = $entry->field("doi");
-		my $volume  = $entry->field("volume");
-		my $number  = $entry->field("number");
+		# remember to remove extraneous {} from title
+		$title[$counter]   = $entry->field("title");
+		$journal[$counter] = $entry->field("journal");
+		$doi[$counter]     = $entry->field("doi");
+		$volume[$counter]  = $entry->field("volume");
+		$number[$counter]  = $entry->field("number");
 		my @authors = $entry->author;
-		
-		
-		print $type . "\n";
-		print $title . "\n";
-		print $journal . ", " . $volume . "(" . $number . ")\n";
+		foreach my $authTemp (@authors) {
+			$authorList[$counter] .= $authTemp->last;
+			$authorList[$counter] .= ", ";
+			$authorList[$counter] .= $authTemp->first;
+			# if $authTemp is the last element, skip next line
+			# or put a period instead
+			$authorList[$counter] .= ", ";
+		}
+		$counter++;
+		# print $type . "\n";
+		# print $title . "\n";
+		# print $journal . ", " . $volume . "(" . $number . ")\n";
 		# need check for missing values for volume and number
 		# as well as pages if you implement that
-		print "http://dx.doi.org/" . $doi . "\n";
-		foreach my $author (@authors) {
-			print $author->first . " " . $author->last . ", ";
+		# print "http://dx.doi.org/" . $doi . "\n";
+		# foreach my $author (@authors) {
+			# print $author->first . " " . $author->last . ", ";
 			# put in a thing to check if it's the last item, to remove comma
-		}
+		# }
 		print "\n";
 	} else {
 		warn "Error parsing file: " . $entry->error;
 	}
 
 }
+
+$counter = 0;
+while ($counter < 4){
+	print $TF $title[$counter] . "\n";
+	print $TF $authorList[$counter] . "\n";
+	print $TF $journal[$counter] . "\n\n";
+	$counter++;
+}
+
 
 # Generating the HTML in the manner below
 # appears to work fine. (2015-05-05)
